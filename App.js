529db8031e9e70357f16124f78ddb856f065d209
bully82 <goladzo@outlook.com>
2022-11-21 10:02:57 +0000

    commit

--- App.js
+++ App.js
@@ -8,11 +8,10 @@ import { withAuthenticator } from "aws-amplify-react-native";
 Amplify.configure(awsconfig);
 
 import * as SplashScreen from "expo-splash-screen";
-import { Text, StyleSheet, StatusBar } from "react-native";
+import { Text, StyleSheet, View, StatusBar, SafeAreaView } from "react-native";
 import { useFonts } from "expo-font";
 //Navigation
 import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
 
 function App() {
@@ -35,21 +34,20 @@ function App() {
   }
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <NavigationContainer>
-        <StatusBar style="auto" />
-        <BottomTabNavigator />
-      </NavigationContainer>
-    </SafeAreaView>
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <BottomTabNavigator />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    overflow: "hidden",
-    marginTop: StatusBar.currentHeight,
-  },
-});
+// const styles = StyleSheet.create({
+//   BottomTab: {
+//     flex: 1,
+//     justifyContent: "flex-end",
+//     //     overflow: "hidden",
+//     //     marginTop: StatusBar.currentHeight,
+//   },
+// });
 
 export default withAuthenticator(App);
